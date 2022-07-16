703aa939110080a5db0f505ae41518be5f3d2e7d
emdadhossain25 <emdad@ergo-ventures.com>
2022-07-16 20:07:22 +0600

    added nav host

--- app/src/main/java/com/example/imagesearchapplication/data/UnsplashPhoto.kt
+++ app/src/main/java/com/example/imagesearchapplication/data/UnsplashPhoto.kt
@@ -1,7 +1,7 @@
 package com.example.imagesearchapplication.data
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UnsplashPhoto(
