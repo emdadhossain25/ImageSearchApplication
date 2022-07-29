92610111dad017d5a93fc40e5fc3366aa3fb7e38
emdadhossain25 <emdad@ergo-ventures.com>
2022-07-30 01:43:05 +0600

    added search functionlay for galleryfragment

--- app/src/main/java/com/example/imagesearchapplication/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearchapplication/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.example.imagesearchapplication.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.imagesearchapplication.R
@@ -28,6 +31,31 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_search_gallery, menu)
+
+        var searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus() // Hides keyboard
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true //should not search while text input
+            }
+
+        })
     }
 
     override fun onDestroyView() {
